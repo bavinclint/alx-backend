5ad54f08394b016371381fd8a35abb852a0c4013
bavinclint <bavinclint@gmail.com>
2023-04-19 22:57:58 +0300

    MRU caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,47 +1,33 @@
-#!/usr/bin/python3
-""" Module for class FIFOCache"""
-
+#!/usr/bin/env python3
+"""First-In First-Out caching module.
+"""
+from collections import OrderedDict
 
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """ inherits from BaseCaching and is a caching system """
-
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with a FIFO
+    removal mechanism when the limit is reached.
+    """
     def __init__(self):
-        """
-        constructor for FIFOCache that calls parent's constuctor
+        """Initializes the cache.
         """
         super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """
-        Add an item in the cache, but
-        if the no:of items in the dictionary is greater
-        than BaseCaching.MAX_ITEMS the 1st item should
-        be discarded as for the fifo algorithm
+        """Adds an item in the cache.
         """
         if key is None or item is None:
             return
-
         self.cache_data[key] = item
-
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            # next(iter(dic)) removes 1st item
-            # dict keeps insertion order
-            key = next(iter(self.cache_data))
-            del self.cache_data[key]
-            print("DISCARD: {}".format(key))
+            first_key, _ = self.cache_data.popitem(False)
+            print("DISCARD:", first_key)
 
-        def get(self, key):
-            """
-            Get an item by key
-            """
-            if key is None:
-                None
-
-            elif key not in self.cache_data:
-                None
-
-            else:
-                return self.cache_data.get(key)
+    def get(self, key):
+        """Retrieves an item by key.
+        """
+        return self.cache_data.get(key, None)
